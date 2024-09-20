33851c805a2f18fcb3508179df3e6fef61f1dba9
Miftahulislam690 <miftahulislam.info@gmail.com>
2024-09-20 16:24:14 +0600

    added the more code

--- js/home.js
+++ js/home.js
@@ -3,5 +3,15 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     // console.log('add money btn is added');
     const inputAddMoney = document.getElementById('input-add-money').value;
     const inputPinNumber = document.getElementById('input-pin-number').value;
-    console.log(inputAddMoney, inputPinNumber);
+    if(inputPinNumber === '5555'){
+        const balanceNumber = document.getElementById('account-balance').innerText;
+        // console.log(balance);
+        const addMoneyNumber = parseFloat(inputAddMoney);
+        const balanceInputNumber = parseFloat(balanceNumber);
+        const newBalance = addMoneyNumber + balanceInputNumber;
+        // console.log(newBalance);
+        const addMoney = document.getElementById('account-balance').innerText = newBalance;
+    }else{
+        alert('Failed to adding money');
+    }
 })
